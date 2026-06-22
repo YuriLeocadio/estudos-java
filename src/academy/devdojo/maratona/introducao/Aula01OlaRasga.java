8a7ece3b66aa795c06e51602f3a281dea2169805
YuriLeocadio <yurileocadio0411@gmail.com>
2026-06-22 11:08:14 -0300

    B ajustando gitignore e adicionando arquivos do curso

---
+++ src/academy/devdojo/maratona/introducao/Aula01OlaRasga.java
@@ -0,0 +1,7 @@
+package academy.devdojo.maratona.introducao;
+
+public class Aula01OlaRasga {
+    public static void main(String[] args){
+        System.out.println("KA-ME-HA-ME-HAAAA");
+    }
+}
---
+++ src/academy/devdojo/maratona/introducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,9 @@
+package academy.devdojo.maratona.introducao;
+
+public class Aula02TiposPrimitivos {
+    public static void main(String[] args) {
+        // int, double, float, char, byte, short, long, boolean
+        int idade = 18;
+        System.out.println(idade);
+    }
+}
