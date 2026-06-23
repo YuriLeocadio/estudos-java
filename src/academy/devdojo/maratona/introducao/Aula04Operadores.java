597c1e8c2c11dc01b22fb93c595d0027b279f841
YuriLeocadio <yurileocadio0411@gmail.com>
2026-06-23 12:02:53 -0300

    Aula de operadores finalizada

--- src/academy/devdojo/maratona/introducao/Aula04Operadores.java
+++ src/academy/devdojo/maratona/introducao/Aula04Operadores.java
@@ -1,4 +1,61 @@
 package academy.devdojo.maratona.introducao;
 
 public class Aula04Operadores {
+    public static void main(String[] args) {
+        // Aritiméticos: + - * /
+        int numero01 = 10;
+        double numero02 = 20;
+        double resultado = numero01 / numero02;
+        System.out.println(resultado);
+
+        // Resto: %
+        int resto = 20 % 2;
+        System.out.println(resto);
+
+        // Relacionais: < > <= >= == !=
+        boolean isDezMaiorQueVinte = 10 > 20;
+        boolean isDezMenorQueVinte = 10 < 20;
+        boolean isDezIgualVinte = 10 == 20;
+        boolean isDezIgualDez = 10 == 10;
+        boolean isDezDiferenteDez = 10 != 10;
+        System.out.println("isDezMaiorQueVinte " + isDezMaiorQueVinte);
+        System.out.println("isDezMenorQueVinte " + isDezMenorQueVinte);
+        System.out.println("isDezIgualVinte " + isDezIgualVinte);
+        System.out.println("isDezIgualDez " + isDezIgualDez);
+        System.out.println("isDezDiferenteDez " + isDezDiferenteDez);
+
+        // Lógicos: &&(AND) ||(OR) !(NOT)
+        int idade = 29;
+        float salario = 3500F;
+        boolean isDentroDaLeiMaiorQueTrinta = idade >= 30 && salario >= 4612;
+        boolean isDentroDaLeiMenorQueTrinta = idade < 30 && salario >= 3381;
+        System.out.println("isDentroDaLeiMaiorQueTrinta " + isDentroDaLeiMaiorQueTrinta);
+        System.out.println("isDentroDaLeiMenorQueTrinta " + isDentroDaLeiMenorQueTrinta);
+
+        double valorTotalContaCorrente = 200;
+        double valorTotalContaPoupanca = 10000;
+        float valorPlaystation = 5000F;
+        boolean isPlaystationCincoCompravel = valorTotalContaCorrente >= valorPlaystation || valorTotalContaPoupanca >= valorPlaystation;
+        System.out.println("isPlaystationCincoCompravel " + isPlaystationCincoCompravel);
+
+        // Atribuição: = += -= *= /= %=
+        double bonus = 1800;
+        bonus += 1000;
+        bonus -= 1000;
+        bonus *= 2;
+        bonus /= 2;
+        bonus %= 2;
+        System.out.println("bonus " + bonus);
+
+        // Incremento e Decremento: ++ --
+        int contador = 0;
+        contador += 1;
+        contador++;
+        contador--;
+        ++contador;
+        --contador;
+        int contador2 = 0;
+        System.out.println(contador2++);
+        System.out.println(contador2);
+    }
 }
