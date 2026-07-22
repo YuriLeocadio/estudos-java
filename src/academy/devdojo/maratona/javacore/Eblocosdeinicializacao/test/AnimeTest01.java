b58089eaf0e0d23214b8fb411920f3914ec8b070
YuriLeocadio <yurileocadio0411@gmail.com>
2026-07-22 06:30:56 -0300

    feat: Implementa prática de blocos de inicialização

---
+++ src/academy/devdojo/maratona/javacore/Eblocosdeinicializacao/test/AnimeTest01.java
@@ -0,0 +1,14 @@
+package academy.devdojo.maratona.javacore.Eblocosdeinicializacao.test;
+
+import academy.devdojo.maratona.javacore.Eblocosdeinicializacao.dominio.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime();
+
+        for (int episodio : anime.getEpisodios()) {
+            System.out.print(episodio + " ");
+        }
+
+    }
+}
