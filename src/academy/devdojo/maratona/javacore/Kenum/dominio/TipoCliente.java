e477326a9d4baffb1cc426d6132f4b8e0c59352d
YuriLeocadio <yurileocadio0411@gmail.com>
2026-07-31 06:18:02 -0300

    feat: implementa enumeração
    Cobre construtores, atributos, sobrescrita de métodos e busca por atributos

---
+++ src/academy/devdojo/maratona/javacore/Kenum/dominio/TipoCliente.java
@@ -0,0 +1,30 @@
+package academy.devdojo.maratona.javacore.Kenum.dominio;
+
+public enum TipoCliente {
+    PESSOA_FISICA(1, "Pessoa Física"),
+    PESSOA_JURIDICA(2, "Pessoa Jurídica");
+    private int valor;
+    private String descricao;
+
+    TipoCliente(int valor, String descricao) {
+        this.valor = valor;
+        this.descricao = descricao;
+    }
+
+    public static TipoCliente tipoClientePorDescricao(String descricao){
+        for (TipoCliente tipoCliente : values()) {
+            if (tipoCliente.getDescricao().equals(descricao)) {
+                return tipoCliente;
+            }
+        }
+        return null;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+}
---
+++ src/academy/devdojo/maratona/javacore/Kenum/test/ClienteTest01.java
@@ -0,0 +1,23 @@
+package academy.devdojo.maratona.javacore.Kenum.test;
+
+import academy.devdojo.maratona.javacore.Kenum.dominio.Cliente;
+import academy.devdojo.maratona.javacore.Kenum.dominio.TipoCliente;
+import academy.devdojo.maratona.javacore.Kenum.dominio.TipoPagamento;
+
+import java.sql.SQLOutput;
+
+public class ClienteTest01 {
+    public static void main(String[] args) {
+        Cliente cliente1 = new Cliente("Tsubasa", TipoCliente.PESSOA_FISICA,TipoPagamento.DEBITO);
+        Cliente cliente2 = new Cliente("Wakabayashi", TipoCliente.PESSOA_JURIDICA, TipoPagamento.CREDITO);
+
+        System.out.println(cliente1);
+        System.out.println(cliente2);
+        System.out.println(TipoPagamento.DEBITO.calcularDesconto(100));
+        System.out.println(TipoPagamento.CREDITO.calcularDesconto(100));
+        TipoCliente tipoCliente = TipoCliente.valueOf("PESSOA_FISICA");
+        System.out.println(tipoCliente.getDescricao());
+        TipoCliente tipoCliente2 = TipoCliente.tipoClientePorDescricao("Pessoa Física");
+        System.out.println(tipoCliente2);
+    }
+}
