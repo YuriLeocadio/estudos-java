2a17f01ecff3b64bffbf8fa5abd642c887598d83
YuriLeocadio <yurileocadio0411@gmail.com>
2026-08-01 05:50:03 -0300

    feat: implementa classes abstratas
    Cobre conceito, métodos abstratos e regras dos métodos abstratos

---
+++ src/academy/devdojo/maratona/javacore/Lclassesabstratas/test/FuncionarioTest01.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratona.javacore.Lclassesabstratas.test;
+
+import academy.devdojo.maratona.javacore.Lclassesabstratas.dominio.Desenvolvedor;
+import academy.devdojo.maratona.javacore.Lclassesabstratas.dominio.Funcionario;
+import academy.devdojo.maratona.javacore.Lclassesabstratas.dominio.Gerente;
+
+public class FuncionarioTest01 {
+    public static void main(String[] args) {
+        Gerente gerente = new Gerente("Nami", 5000);
+        Desenvolvedor desenvolvedor = new Desenvolvedor("Megumi", 12000);
+        System.out.println(gerente);
+        System.out.println(desenvolvedor);
+        gerente.imprime();
+        desenvolvedor.imprime();
+    }
+}
