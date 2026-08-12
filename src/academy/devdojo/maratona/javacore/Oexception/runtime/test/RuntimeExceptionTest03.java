3813257a243d4f54a3ff850c5c0a3e97777afe62
YuriLeocadio <yurileocadio0411@gmail.com>
2026-08-12 08:58:28 -0300

    feat: implementa exception
    Cobre bloco finally e tratamento de múltiplas exceções

---
+++ src/academy/devdojo/maratona/javacore/Oexception/runtime/test/RuntimeExceptionTest03.java
@@ -0,0 +1,29 @@
+package academy.devdojo.maratona.javacore.Oexception.runtime.test;
+
+public class RuntimeExceptionTest03 {
+    public static void main(String[] args) {
+        abreConexao();
+    }
+
+    public static String abreConexao() {
+        try {
+            System.out.println("Abrindo arquivo");
+            System.out.println("Escrevendo dados no arquivo");
+            return "conexão aberta";
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("Fechando recurso liberado pelo SO");
+        }
+        return null;
+    }
+
+    public static void abreConexao2() {
+        try {
+            System.out.println("Abrindo arquivo");
+            System.out.println("Escrevendo dados no arquivo");
+        } finally {
+            System.out.println("Fechando recurso liberado pelo SO");
+        }
+    }
+}
